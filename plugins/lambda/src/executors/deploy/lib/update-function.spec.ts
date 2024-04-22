18ed4b8e3c1c06b067556dc8823ed068a47a9205
Felix Korovin <felix@captain-eye.com>
2024-04-22 14:18:59 +0300

    test: refactored update-function tests

--- plugins/lambda/src/executors/deploy/lib/update-function.spec.ts
+++ plugins/lambda/src/executors/deploy/lib/update-function.spec.ts
@@ -1,51 +1,64 @@
 import { updateFunction } from './update-function';
 import { readFile } from 'fs/promises';
-import { Lambda } from '@aws-sdk/client-lambda';
+import {
+  LambdaClient,
+  UpdateFunctionCodeCommand,
+} from '@aws-sdk/client-lambda';
+import { mockClient } from 'aws-sdk-client-mock';
+import 'aws-sdk-client-mock-jest';
+import * as mockFs from 'mock-fs';
 
-jest.mock('fs/promises', () => ({
-  readFile: jest.fn(),
-}));
-
-jest.mock('@aws-sdk/client-lambda', () => ({
-  Lambda: jest.fn().mockImplementation(() => ({
-    updateFunctionCode: jest.fn(),
-  })),
-}));
+const lambdaMock = mockClient(LambdaClient);
 
 describe('updateFunction', () => {
-  afterEach(() => {
-    jest.clearAllMocks();
+  beforeEach(() => {
+    mockFs({
+      'package.zip': 'packageContents',
+    });
   });
 
-  it('should read the package file if no s3 bucket is provided', () => {
-    (readFile as jest.Mock).mockResolvedValueOnce('packageContents');
+  afterEach(() => {
+    lambdaMock.reset();
+    mockFs.restore();
+  });
 
-    (Lambda as jest.Mock).mockImplementationOnce(() => ({
-      updateFunctionCode: jest.fn().mockResolvedValueOnce({
-        FunctionArn: 'FunctionArn',
-      }),
-    }));
-
+  it('should fail if updating from local file and the file is missing', async () => {
+    // Arrange
     const options = {
       functionName: 'functionName',
-      packageFilePath: 'packageFilePath',
+      packageFilePath: 'wrong_package.zip',
       publish: true,
     };
 
-    expect(updateFunction(options)).resolves.toEqual({
-      FunctionArn: 'FunctionArn',
-    });
+    expect.assertions(2);
 
-    expect(readFile).toHaveBeenCalledWith('packageFilePath');
+    try {
+      // Act
+      await updateFunction(options);
+    } catch (err) {
+      // Assert
+      expect(err.message).toMatch(/no such file or directory/);
+      expect(lambdaMock).not.toHaveReceivedAnyCommand();
+    }
   });
 
-  it('should not read the package file if an s3 bucket is provided', () => {
-    (Lambda as jest.Mock).mockImplementationOnce(() => ({
-      updateFunctionCode: jest.fn().mockResolvedValueOnce({
-        FunctionArn: 'FunctionArn',
-      }),
-    }));
+  it('should update function code from local file', async () => {
+    // Arrange
+    const options = {
+      functionName: 'functionName',
+      packageFilePath: 'package.zip',
+      publish: true,
+    };
 
+    // Act
+    await updateFunction(options);
+
+    // Assert
+    expect(lambdaMock).toHaveReceivedCommand(UpdateFunctionCodeCommand);
+  });
+
+  it('should update function code from S3', async () => {
+    // Arrange
     const options = {
       functionName: 'functionName',
       s3Bucket: 's3Bucket',
@@ -53,26 +66,10 @@ describe('updateFunction', () => {
       publish: true,
     };
 
-    expect(updateFunction(options)).resolves.toEqual({
-      FunctionArn: 'FunctionArn',
-    });
+    // Act
+    await updateFunction(options);
 
-    expect(readFile).not.toHaveBeenCalled();
-  });
-
-  it('should reject if updateFunctionCode rejects', () => {
-    (readFile as jest.Mock).mockResolvedValueOnce('packageContents');
-
-    (Lambda as jest.Mock).mockImplementationOnce(() => ({
-      updateFunctionCode: jest.fn().mockRejectedValueOnce(new Error('error')),
-    }));
-
-    const options = {
-      functionName: 'functionName',
-      packageFilePath: 'packageFilePath',
-      publish: true,
-    };
-
-    return expect(updateFunction(options)).rejects.toThrow('error');
+    // Assert
+    expect(lambdaMock).toHaveReceivedCommand(UpdateFunctionCodeCommand);
   });
 });
